2e0212237689e36f457a9e003ea271a3fe15fdd9
Michał Szymczyk <michalszymczyk@MacBook-Air-Michal-2.local>
2017-05-11 20:13:20 +0200

    add

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+		navigation: true,
+		navigationPosition: 'right',
+		slidesNavigation: true,
+		slidesNavPosition: 'bottom',
+		controlArrows: false,
+		verticalCentered: true,
+		paddingTop: '3em',
+		paddingBottom: '10px',
+		fixedElements: '#header',
+		responsiveWidth: 0,
+		responsiveHeight: 0,
+		scrollingSpeed: 300,	
+
+	 });
+});
+
+
+
+
+    
